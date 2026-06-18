335a0f5ff5d1171bbf8c049a6f63d0d8c2879d90
Sezu-Dumoju <vvasiliev237@gmail.com>
2026-06-18 11:58:00 +0200

    add basic auth

--- Task7Template/index.js
+++ Task7Template/index.js
@@ -7,7 +7,12 @@ const { staticFile } = require('./utility/static_file');
 const PORT = process.env.PORT || 3500;
 
 http.createServer(function (req, res) {
-
+    const auth = req.headers['authorization'];
+    if (!auth || auth !== 'Basic ' + Buffer.from('admin:1234').toString('base64')) {
+        res.writeHead(401, { 'WWW-Authenticate': 'Basic realm="Login"' });
+        res.end('Unauthorized');
+        return;
+    }
     const url = req.url;
 
     switch (url) {
