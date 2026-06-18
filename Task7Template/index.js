e2ae40000b7e6af21ab46167efc28fbfa4eb5d46
Sezu-Dumoju <vvasiliev237@gmail.com>
2026-06-18 12:00:48 +0200

    add basic auth

--- Task7Template/index.js
+++ Task7Template/index.js
@@ -1,11 +1,8 @@
 const http = require('http');
 const path = require('path');
-
 const { mimeTypes } = require('./utility/mime');
 const { staticFile } = require('./utility/static_file');
-
 const PORT = process.env.PORT || 3500;
-
 http.createServer(function (req, res) {
     const auth = req.headers['authorization'];
     if (!auth || auth !== 'Basic ' + Buffer.from('admin:1234').toString('base64')) {
@@ -14,17 +11,12 @@ http.createServer(function (req, res) {
         return;
     }
     const url = req.url;
-
     switch (url) {
-
         case '/':
             staticFile(res, '/html/main_page.html', '.html');
             break;
-
         default:
-
             const extname = String(path.extname(url)).toLowerCase();
-
             if (extname in mimeTypes) {
                 staticFile(res, url, extname);
             } else {
@@ -32,5 +24,4 @@ http.createServer(function (req, res) {
                 res.end();
             }
     }
-
 }).listen(PORT);
